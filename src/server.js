7f5a4dc564c2a3428c14c8e05dec83006532fff8
DexterKibs1 <kibiradennis@gmail.com>
2026-05-03 23:45:47 +0300

    fix public folder path

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve frontend
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // API routes
 app.use("/api/packages", require("./routes/packages"));
@@ -18,7 +18,7 @@ app.use("/api/auth", require("./routes/auth"));
 
 // Fallback to index.html (important for frontend routing)
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 // Port (Render compatible)
